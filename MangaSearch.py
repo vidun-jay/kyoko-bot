1df6c092ca15707bcf165eed32e2d5b0fcb96d0f
Jason <jasonkboy0098@gmail.com>
2023-07-17 22:02:55 -0400

    First step in manga feature

---
+++ MangaSearch.py
@@ -0,0 +1,9 @@
+import discord
+from discord import SyncWebhook
+from discord.utils import get
+from dotenv import load_dotenv
+import os
+import requests
+from bs4 import BeautifulSoup
+from urlextract import URLExtract
+
